92f5da6c0af386c18c2d1ade78fe369f0f4ef8ef
zhangxx0 <zxx377241804@gmail.com>
2022-01-07 13:57:26 +0800

    使用nacos做注册和配置中心

--- server1/src/main/java/com.xinxin.product/controller/ServerController.java
+++ server1/src/main/java/com.xinxin.product/controller/ServerController.java
@@ -1,6 +1,8 @@
 package com.xinxin.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,19 @@ public class ServerController {
     @GetMapping("/msg")
     public String msg() {
         return "this is product' msg";
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    /**
+     * feign loadbanlance负载均衡测试
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/loadbanlanceTest")
+    public String loadbanlanceTest(@RequestParam(value = "name",required = false) String name) {
+        return "hello " + name + ", i'm provider ,my port:" + port;
     }
 }
